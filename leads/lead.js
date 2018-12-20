d5c28f892e2ae04e5d278a22ab9da8d2bb976258
Julia Rodrigues <srodriguesjulia@gmail.com>
2018-12-20 18:42:43 -0200

    Inserir página de contato

--- leads/lead.js
+++ leads/lead.js
@@ -5,7 +5,7 @@ const mongoose = restful.mongoose
 let LeadSchema = new mongoose.Schema({
     _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
     name: { type:String, required: true},
-    phone: { type:Number, required: true},
+    phone: { type:Number, required: true}
 })
 
 let leads = restful.model("leads", LeadSchema)
--- portages/portage.js
+++ portages/portage.js
@@ -4,21 +4,26 @@ const mongoose = restful.mongoose
 
 let portageSchema = new mongoose.Schema({
     _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
-    origin: {    
-    address: { type:String, required: true },
-    propertyType: { type:String, required: true },
-    apartment: { type:String, required: false },
-    hasLift: { type:Boolean, required: false },
-    stage: { type:String, required: false },
-    comments: { type:String, required: false }
+    contact: {
+        name: { type: String, required: true },
+        phone: { type: Number, required: true },
+        email: { type: String, required: true }
+    },
+    origin: {  
+        address: { type:String, required: true },
+        propertyType: { type:String, required: true },
+        apartment: { type:String, required: false },
+        hasLift: { type:Boolean, required: false },
+        stage: { type:String, required: false },
+        comments: { type:String, required: false }
     },
     destination:{
-    address: { type:String, required: true },
-    propertyType: { type:String, required: true },
-    apartment: { type:String, required: false },
-    hasLift: { type:Boolean, required: false },
-    stage: { type:String, required: false },
-    comments: { type:String, required: false }
+        address: { type:String, required: true },
+        propertyType: { type:String, required: true },
+        apartment: { type:String, required: false },
+        hasLift: { type:Boolean, required: false },
+        stage: { type:String, required: false },
+        comments: { type:String, required: false }
     },
     distance: {type: String, required: true },
     date: {
