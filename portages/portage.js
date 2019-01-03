c49e469372193b940409206b9f8fb3bdf32dbc65
Julia Rodrigues <srodriguesjulia@gmail.com>
2019-01-03 21:23:03 -0200

    Alterar obrigatoriedade de todos os campos

--- portages/portage.js
+++ portages/portage.js
@@ -6,35 +6,35 @@ let portageSchema = new mongoose.Schema({
     _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
     contact: {
         name: { type: String, required: true },
-        phone: { type: Number, required: false },
-        email: { type: String, required: false }
+        phone: { type: Number, required: true },
+        email: { type: String, required: true }
     },
     origin: {  
-        address: { type:String, required: false },
-        propertyType: { type:String, required: false },
+        address: { type:String, required: true },
+        propertyType: { type:String, required: true },
         apartment: { type:String, required: false },
         hasLift: { type:Boolean, required: false },
         stage: { type:String, required: false },
         comments: { type:String, required: false }
     },
     destination:{
-        address: { type:String, required: false },
-        propertyType: { type:String, required: false },
+        address: { type:String, required: true },
+        propertyType: { type:String, required: true },
         apartment: { type:String, required: false },
         hasLift: { type:Boolean, required: false },
         stage: { type:String, required: false },
         comments: { type:String, required: false }
     },
-    distance: {type: String, required: false },
+    distance: {type: String, required: true },
     date: {
         startDate: {type: String, required: true },
         endDate: {type: String, required: true }
     },
     period: {
-        hour: {type: String, required: false },
+        hour: {type: String, required: true },
         comments: {type: String, required: false }
     },
-    listOfItems: { type: Array, required: false }
+    listOfItems: { type: Array, required: true }
 })
 
 let portage = restful.model("portage", portageSchema)
