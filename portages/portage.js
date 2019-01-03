9585339650fe9ea969adb956e0c2b17ac91c0b5e
Julia Rodrigues <srodriguesjulia@gmail.com>
2019-01-03 21:30:30 -0200

    Alterar nome de campo no banco

--- portages/portage.js
+++ portages/portage.js
@@ -27,8 +27,8 @@ let portageSchema = new mongoose.Schema({
     },
     distance: {type: String, required: true },
     date: {
-        startDate: {type: String, required: true },
-        endDate: {type: String, required: true }
+        start: {type: String, required: true },
+        end: {type: String, required: true }
     },
     period: {
         hour: {type: String, required: true },
